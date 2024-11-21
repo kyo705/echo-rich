8b8c95cf82bb2f63f1b5342306fb98cc3cdae281
kyo705 <mission0705@naver.com>
2024-11-21 21:50:03 +0900

    [INIT] 프로젝트 생성

---
+++ task/src/test/java/com/echoandrich/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echoandrich.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
